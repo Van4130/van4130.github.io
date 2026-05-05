a8047f60e54c7847f3b380739418c16e924f3e91
Van4130 <7661828+Van4130@users.noreply.github.com>
2026-05-05 14:02:08 -0400

    Change headers, add text, develop leaflet map.
    + Change h2 to h1 for project pages and move them out of the section tag.
    + Add div class discussion for all project pages.
    + Update text.
    + Add popups and additional circles to Leaflet map.

--- leaflet/main.js
+++ leaflet/main.js
@@ -6,7 +6,43 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
   maxZoom: 20
 }).addTo(map);
 
+// Morrill
 L.circle([42.38959, -72.52439], { radius: 300, color: 'blue', opacity: 1,    fillColor: 'yellow', fillOpacity: .05 }).addTo(map);
 L.circle([42.38959, -72.52439], { radius: 200, color: 'blue', opacity: .75,  fillColor: 'yellow', fillOpacity: .05 }).addTo(map);
 L.circle([42.38959, -72.52439], { radius: 100, color: 'blue', opacity: .5,   fillColor: 'yellow', fillOpacity: .05 }).addTo(map);
 L.circle([42.38959, -72.52439], { radius: 50,  color: 'blue', opacity: .25,  fillColor: 'yellow', fillOpacity: .05 }).addTo(map);
+// Lot 12
+L.circle([42.3918, -72.5362], { radius: 50,  color: 'blue', opacity: .25,  fillColor: 'yellow', fillOpacity: .05 }).addTo(map);
+
+
+// Ref https://leafletjs.com/examples/quick-start/
+
+// Pop-ups
+var lot12pop = L.popup()
+  .setLatLng([42.3918, -72.5362])
+  .setContent("Lot 12. Here be dragons!")
+  .openOn(map);
+
+var morrillpop = L.popup()
+  .setLatLng([42.38959, -72.52439])
+  .setContent("Morrill Science Center. Here be confusing.")
+  .openOn(map);
+
+var lot12 = L.circle([42.3918, -72.5362], {
+  radius: 50,
+  color: 'blue',
+  fillColor: 'yellow',
+  fillOpacity: 0.05
+}).addTo(map);
+
+var morrill = L.circle([42.38959, -72.52439], {
+  radius: 50,
+  color: 'blue',
+  fillColor: 'yellow',
+  fillOpacity: .05
+}).addTo(map);
+
+morrill.bindPopup("Morrill Science Center. Here be confusing.").openPopup();
+lot12.bindPopup("Lot 12. Here be dragons!").openPopup();
+
+map.on('click', onMapClick);
